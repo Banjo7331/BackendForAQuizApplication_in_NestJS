abc06d0ec696e536376fad3ac6e73fa7d6cbadd9
Bartosz Cylwik <cylwikbartosz@gmail.com>
2024-02-29 00:28:49 +0100

    Config file .env to read dependencies to database Reading from config file database Task: #11

--- src/app.module.ts
+++ src/app.module.ts
@@ -8,22 +8,32 @@ import { QuizAttempt } from './typeorm/entities/QuizAttempt';
 import { Question } from './typeorm/entities/Question';
 import { UserAnswer } from './typeorm/entities/UserQuestionAnswerInput';
 import { QuizAttemptModule } from './quizAttempt/quizAttempt.module';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import databaseConfig from './config/database.config';
 
 @Module({
-  imports: [GraphQLModule.forRoot<ApolloDriverConfig>({
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+      load: [databaseConfig],
+    }),
+    GraphQLModule.forRoot<ApolloDriverConfig>({
     driver: ApolloDriver,
     autoSchemaFile: 'src/schema.gql',
 
   }),
-  TypeOrmModule.forRoot({
-    type: 'postgres',
-    host: 'localhost',//postgres_db',
-    port: 5432,
-    username: 'testuser',
-    password: 'testuser123',
-    database: 'post_new_structure',
-    entities: [Quiz,QuizAttempt,UserAnswer,Question],
-    synchronize: true,
+  TypeOrmModule.forRootAsync({
+    useFactory: (configService: ConfigService) => ({
+      type: 'postgres',
+      host: configService.get('database.host'),//postgres_db'
+      port: configService.get('database.port'),
+      username: configService.get('database.username'),
+      password: configService.get('database.password'),
+      database: configService.get('database.database'),
+      entities: [Quiz, QuizAttempt, UserAnswer, Question],
+      synchronize: true,
+    }),
+    inject: [ConfigService],
   }),
   QuizModule,
   QuizAttemptModule],
